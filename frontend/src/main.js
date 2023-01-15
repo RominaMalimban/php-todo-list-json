834f238dca65b14f200a5be9c5ae53d6e4c0ed44
RominaMalimban <romina.lyn@hotmail.it>
2023-01-15 22:31:50 +0100

    import fontawesome

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import '@fortawesome/fontawesome-free/css/all.css';
+import App from './App.vue';
 
-import App from './App.vue'
-
-createApp(App).mount('#app')
+createApp(App).mount('#app');
