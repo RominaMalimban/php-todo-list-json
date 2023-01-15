4685d6863211595b297fd0504ab9d8ef73812ade
RominaMalimban <romina.lyn@hotmail.it>
2023-01-15 20:47:42 +0100

    aggiunta condizione per toggle task

--- backend/completed.php
+++ backend/completed.php
@@ -16,8 +16,11 @@ $todoList = json_decode($jsonTodoList);
 // seleziono l'oggetto task corrispondente all'indice $index all'interno dell'array $todoList:
 $task = $todoList[$index];
 
-// accedo a proprietà dell'oggeto cambio il valore di "completed" del task selezionato in true:
-$task->completed = true;
-
+// accedo a proprietà dell'oggeto cambio il valore di "completed" del task selezionato con condizione: se true diventa false e viceversa:
+if ($task->completed === true) {
+    $task->completed = false;
+} else {
+    $task->completed = true;
+}
 $jsonTodoList = json_encode($todoList);
 file_put_contents("todo.json", $jsonTodoList);
